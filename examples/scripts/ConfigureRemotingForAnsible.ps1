028168b3f74a28a85edcd4f7567642e3d601554e
Pavel Balcarek <pavel.balcarek@gmail.com>
2021-07-27 11:13:02 +0200

    Scrips: remoting - refactoring - https
    Move related code to function Enable-GlobalHttpsFirewallAccess

--- examples/scripts/ConfigureRemotingForAnsible.ps1
+++ examples/scripts/ConfigureRemotingForAnsible.ps1
@@ -216,6 +216,29 @@ Function Enable-GlobalHttpFirewallAccess
     Write-Verbose "HTTP firewall rule $($rule.Name) updated"
 }
 
+Function Enable-GlobalHttpsFirewallAccess
+{
+    # Configure firewall to allow WinRM HTTPS connections.
+    $fwtest1 = netsh advfirewall firewall show rule name="Allow WinRM HTTPS"
+    $fwtest2 = netsh advfirewall firewall show rule name="Allow WinRM HTTPS" profile=any
+    If ($fwtest1.count -lt 5)
+    {
+        Write-Verbose "Adding firewall rule to allow WinRM HTTPS."
+        netsh advfirewall firewall add rule profile=any name="Allow WinRM HTTPS" dir=in localport=5986 protocol=TCP action=allow
+        Write-Log "Added firewall rule to allow WinRM HTTPS."
+    }
+    ElseIf (($fwtest1.count -ge 5) -and ($fwtest2.count -lt 5))
+    {
+        Write-Verbose "Updating firewall rule to allow WinRM HTTPS for any profile."
+        netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new profile=any
+        Write-Log "Updated firewall rule to allow WinRM HTTPS for any profile."
+    }
+    Else
+    {
+        Write-Verbose "Firewall rule already exists to allow WinRM HTTPS."
+    }
+}
+
 # Setup error handling.
 Trap
 {
@@ -407,25 +430,7 @@ If ($GlobalHttpFirewallAccess) {
     Enable-GlobalHttpFirewallAccess
 }
 
-# Configure firewall to allow WinRM HTTPS connections.
-$fwtest1 = netsh advfirewall firewall show rule name="Allow WinRM HTTPS"
-$fwtest2 = netsh advfirewall firewall show rule name="Allow WinRM HTTPS" profile=any
-If ($fwtest1.count -lt 5)
-{
-    Write-Verbose "Adding firewall rule to allow WinRM HTTPS."
-    netsh advfirewall firewall add rule profile=any name="Allow WinRM HTTPS" dir=in localport=5986 protocol=TCP action=allow
-    Write-Log "Added firewall rule to allow WinRM HTTPS."
-}
-ElseIf (($fwtest1.count -ge 5) -and ($fwtest2.count -lt 5))
-{
-    Write-Verbose "Updating firewall rule to allow WinRM HTTPS for any profile."
-    netsh advfirewall firewall set rule name="Allow WinRM HTTPS" new profile=any
-    Write-Log "Updated firewall rule to allow WinRM HTTPS for any profile."
-}
-Else
-{
-    Write-Verbose "Firewall rule already exists to allow WinRM HTTPS."
-}
+Enable-GlobalHttpsFirewallAccess
 
 # Test a remoting connection to localhost, which should work.
 $httpResult = Invoke-Command -ComputerName "localhost" -ScriptBlock {$env:COMPUTERNAME} -ErrorVariable httpError -ErrorAction SilentlyContinue
